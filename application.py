9ee2304ff56640701dc150cdbdc4b0bf74974df7
samehvirus <samehvirus@hotmail.com>
2019-05-21 17:27:19 +0300

    first commit

---
+++ application.py
@@ -0,0 +1,190 @@
+import os
+
+from flask import Flask, render_template, request, session, url_for, send_from_directory
+from flask_session import Session
+from flask_socketio import SocketIO, emit, join_room, leave_room, rooms, disconnect, send
+from collections import deque
+from werkzeug.utils import secure_filename
+
+
+app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+app.config['UPLOAD_FOLDER'] = './uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+Session(app)
+socketio = SocketIO(app, manage_session=False)
+chat_rooms = ["General Talk"]
+default_room = "General Talk"
+# here we keep track of online users
+# using dictionary, where key is unique session id of every user,
+# and value is username
+active_users = {}
+# here we store last 100 message for every specific chat-room
+# using dictionary, where key is room name,
+# and the value is list of dictionarys( every dictionary contain a message and the sender name). 
+history = {}
+
+@app.route("/")
+def index():
+	return render_template('index.html')
+
+
+@socketio.on('connect')
+def connect():
+	print(session.sid)
+	if 'user' in session:
+		print('connect')
+		print("wellcome, back .. i'll check your credentials")
+		check(session['user'])
+	else:
+		print('connect')
+		print("i'll send auth request to the client")
+		emit('authentication required', 'hello')
+		
+
+@socketio.on('message')
+def pass_message(msg):
+	if 'user' in session:
+		# pass this message to all clients connected in the same room
+		send(msg, room=session['current_room'])
+		# lets keep track of last 100 message in every specifec room
+		# history is a dictionary where kye is room name and value is list of dictionares,
+		# every dictionary in that list represent a message (message body, and sender)
+		if session['current_room'] in history:
+			# make sure to limit message storage to 100 message in each room
+			if len(history[session['current_room']]) >= 100:
+				history[session['current_room']].popleft()
+			history[session['current_room']].append(msg)
+			print(history[session['current_room']])
+		else:
+			# first message in this room.
+			# we will use deque collection to store messages so we can pop old message easy and fast
+			history[session['current_room']] = deque([msg])
+
+
+@socketio.on('authentication request')
+def check(name):
+	if (name):
+		print('start checking the credentials')
+		# TO DO: check if this name currently in active users list
+		print("i'll accept it and log you in")
+		# we call a function to first save the username in the server side session,
+		# and to add user to online-users list, so we can track the number of online users
+		# if this function return false then user did not added as he is already online from other tab
+		res = add_user(name)
+		if res:
+			# lets find a room to the user. we call a function that add new users to default room,
+			# and add old users to last room they was in.
+			switch_room()
+			# now build a list of last 100 message was sent in this room
+			if history.get(session['current_room']):
+				# we need to convert deque collection to list before send it to the client, deque not supported to send in json data
+				last100msg = list(history[session['current_room']])
+			else:
+				last100msg = []
+			# send a loged in event to the client, with a lot of useful info,
+			# number of online users,
+			# list of all rooms,
+			# user current room,
+			# last 100 message in current user room
+			emit('loged in', {'name': session['user'], 'active_users': len(active_users), 'rooms': chat_rooms, 'current_room': session['current_room'], 'last100msg': last100msg})
+			print(f"loged in with name: {session['user']}")
+		else:
+			print('user have other active session')
+
+
+@socketio.on('disconnect')
+def remove_user():
+	if session.sid in active_users:
+		# remove loged in user from online users
+		active_users.pop(session.sid, None)
+		print(session['user'] + 'has disconnected. now users are' + str(len(active_users)))
+	else:
+		# user did not loged in properly yet. so he did not exesit in online users list. no further action required
+		print('no need to remove this client from active users list becouse he did not loged in yet')
+
+
+@socketio.on('request create room')
+def create_room(room):
+	# check if room is valid and not already exists.
+	if room['name'] and room['name'] not in chat_rooms:
+		chat_rooms.append(room['name'])
+		switch_room(room['name'])
+		# send all rooms and current user room to the client, so it can build rooms list and mark the room wich user already in
+		socketio.emit('refresh rooms', {'rooms': chat_rooms}, broadcast=True)
+
+
+def add_user(username):
+	# if user not in server session add it so we can remember him later
+	if 'user' not in session:
+		session['user']=username
+	# if user not in online-users list, add him to the list
+	if session.sid not in active_users:
+		active_users.update({session.sid: username})
+		print(f'added {username} to users\n users are {len(active_users)} ')
+		return True
+	# user is already loged in , this mean he try to open 2nd browser tab 
+	# if this behaviour  allowd , it will cause problem when user close one of the multible tabs,
+	# as disconnect event will log user out or miss-count online-users.
+	else:
+		print('user already loged in')
+		return False
+	# user no 
+
+
+@socketio.on('switch room')
+def switch_room(room=None):
+	if not room:	
+		# user try to log in. let's rememberd last room he was in.
+		if 'current_room' in session and session['current_room'] in chat_rooms:
+			room = session['current_room']
+		# or log him in the server default room , till he choose later other room ( this included when rooms deleted after server restarted)
+		else:	
+			room = default_room
+			session['current_room'] = room
+	elif room in chat_rooms:
+		# user switch current room. so first lets remove him from current room
+		leave_room(session['current_room'])
+		# then make sure to remmember his choise later when he came back
+		session['current_room'] = room
+	else:
+		room = default_room
+		session['current_room'] = room
+	join_room(room)
+	if history.get(session['current_room']):
+		# we need to convert deque collection to list before send it to the client, deque not supported to send in json data
+		last100msg = list(history[session['current_room']])
+	else:
+		last100msg = []
+	emit('joined a room', {'current_room': room, 'last100msg': last100msg})
+	print('user joined room: ' + room)
+
+
+@socketio.on('upload')
+def upload(data):
+	if data['data'] and data['name'] != '' and allowed_file(data['name']):
+		filename = secure_filename(data['name'])
+		with open(os.path.join(app.config['UPLOAD_FOLDER'], filename), "xb") as newFile:
+			newFile.write(data['data'])
+		print(data['name'])
+		print(data['size'])
+		print(data['type'])
+		pass_message({'file': url_for('uploads',filename=filename), 'message': filename, 'sender': session['user']})
+	else:
+		print('not allowed')
+
+@app.route('/uploads/<filename>')
+def uploads(filename):
+	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+if __name__ == '__main__':
+	socketio.run(app, debug = True, host = '0.0.0.0')
---
+++ static/index.js
@@ -0,0 +1,242 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Initialize variables
+  // login page
+  let $loginPage = document.querySelector('#login_page')
+  let $usernameInput = document.querySelector('#name_input')
+  // chat page
+  let $chatPage = document.querySelector('#chat_page')
+  let $roomName = document.querySelector('#room_name')
+  let $msgBox = document.querySelector('#msg_Box')
+  let $msgInput = document.querySelector('#msg_input')
+  let $sendButton = document.querySelector('#send')
+  let $uploadInput = document.querySelector('#upload')
+  let $displayName = document.querySelector('#display_name')
+  let $cards = document.querySelector('#cards')
+  let $createRoom = document.querySelector('#create_room')
+
+  const now = function () {
+    let today = new Date();
+    let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    let dateTime = date + ' ' + time;
+    return dateTime;
+  }
+
+  let userName = null;
+  let logedIn = false;
+  let currentRoom = null;
+  let socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port,
+    {
+      query: {
+      }
+    });
+
+
+  function addUser(username) {
+    // send client username to the server
+    socket.emit('authentication request', username);
+  }
+
+  function send(msg) {
+    if (logedIn) {
+      socket.send({ message: msg, sender: userName });
+    }
+  }
+
+  function notify_user(msg) {
+    const li = document.createElement('li');
+    li.innerHTML = `<small>${now()}</small>  ${msg}`
+    li.style = 'color: goldenrod;'
+    $msgBox.insertBefore(li, $msgBox.firstChild);
+  }
+
+  function add_message(msg) {
+    let li = document.createElement('li');
+    let p = document.createElement('p');
+    p.innerHTML = `<small>${now()}</small> <strong>${msg.sender}</strong> : ${msg.message}`;
+
+    if (msg.file) {
+      let a = document.createElement('a');
+      a.setAttribute('href', msg.file)
+      a.setAttribute('target', '_blank')
+      a.setAttribute('rel', 'noopener noreferrer')
+      a.textContent = " Open this File (only if you trust it)";
+      p.appendChild(a)
+    }
+
+    li.appendChild(p)
+    if (msg.sender !== userName) {
+      li.style = 'color: blue;'
+    }
+    $msgBox.insertBefore(li, $msgBox.firstChild);
+  }
+
+  function refresh_rooms(current = currentRoom, rooms) {
+    console.log(rooms)
+    $cards.innerHTML = '';
+    for (room in rooms) {
+      const button = document.createElement('button');
+      button.innerHTML = `<strong>${rooms[room]}</strong>`
+      button.dataset.room = rooms[room]
+      button.classList.add('room_button')
+      if (rooms[room] == current) {
+        button.style = 'color: green;'
+      }
+      button.addEventListener('click', function (event) {
+        socket.emit('switch room', this.dataset.room)
+        const buttons = document.querySelectorAll(".room_button")
+        for (const button of buttons) {
+          button.style = 'color: black;'
+        }
+        this.style = 'color: green;'
+
+      });
+      $cards.append(button);
+    }
+  }
+
+  function FileChosen(event) {
+    selectedFile = event.target.files[0];
+    console.log(selectedFile)
+    let name = selectedFile.name
+    FReader = new FileReader();
+    FReader.onload = function (evnt) {
+      socket.emit('upload', { 'name': name, 'data': evnt.target.result, 'size': selectedFile.size, 'type': selectedFile.type });
+    }
+    FReader.readAsArrayBuffer(selectedFile);
+
+  }
+  // Keyboard events
+  $usernameInput.addEventListener('keypress', (event) => {
+    if (event.keyCode == 13) {
+      let user_name = $usernameInput.value
+      if (user_name) {
+        addUser(user_name);
+      }
+    }
+  });
+
+  $msgInput.addEventListener('keypress', (event) => {
+    if (event.keyCode == 13) {
+      let msg = $msgInput.value
+      if (msg) {
+        $msgInput.value = ''
+        send(msg);
+      }
+    }
+  });
+
+  // click event
+  $createRoom.addEventListener('click', () => {
+    let room = prompt('give your new channel a name: ');
+    if (room.length > 0) {
+      socket.emit("request create room", { "name": room });
+      // maybe a callback function to make sure server create the room?
+
+    }
+  });
+
+  $sendButton.addEventListener('click', () => {
+    let msg = $msgInput.value
+    if (msg) {
+      $msgInput.value = ''
+      send(msg);
+    }
+  });
+
+  $uploadInput.addEventListener('change', FileChosen);
+
+
+  // socket events
+  socket.on('connect', () => {
+    socket.io.opts.query = {
+      token: 'fgh'
+    }
+    //socket.io.opts.transports = ['websocket'];
+    console.log('connected, socket id is ' + socket.id);
+  });
+
+  socket.on('authentication required', () => {
+    console.log('server sent authentication required event');
+    //socket.io.opts.query = {
+    //  name: 'sameh'
+    //}
+    $loginPage.style.display = 'block';
+    $chatPage.style.display = 'none';
+  });
+
+  socket.on('loged in', (data) => {
+    $loginPage.style.display = 'none';
+    $chatPage.style.display = 'block';
+    console.log('loged in');
+    userName = data.name;
+    logedIn = true;
+    $displayName.textContent = userName
+    // $roomName.textContent = data.current_room;
+    document.querySelector('#status').classList.replace('text-warning', 'text-success');
+    refresh_rooms(data.current_room, data.rooms);
+    // data.last100msg.forEach(msg => {
+    //   add_message(msg)
+    // });
+    notify_user(`You've been logged in successfully, active users : ${data.active_users}`);
+  })
+
+
+  socket.on('joined a room', (room) => {
+    $roomName.textContent = room.current_room;
+    currentRoom = room.current_room;
+    let active_button = document.querySelectorAll(`[data-room~="${room.current_room}"]`);
+    if (active_button) {
+      active_button.style = 'color: green;'
+      console.log(active_button)
+    }
+    $msgBox.innerHTML = ''
+    notify_user(`you are now in ${room.current_room} room`)
+    room.last100msg.forEach(msg => {
+      add_message(msg)
+    });
+    notify_user('all messages below have already been sent before you join this room now.')
+  })
+
+
+  socket.on('message', (msg) => {
+    add_message(msg)
+  });
+
+  socket.on('refresh rooms', (data) => {
+    refresh_rooms(currentRoom, data.rooms)
+  });
+
+  socket.on('connect_error', (error) => {
+    console.log('connection error: ' + error.message);
+  });
+
+  socket.on('reconnect_error', (error) => {
+    console.log('attempt to reconnect has failed! :' + error);
+  });
+
+
+  socket.on('reconnect_attempt', (count) => {
+    console.log('reconnecting .. ' + count);
+  });
+
+  socket.on('reconnect', (count) => {
+    console.log('you have been reconnected after ' + count + 'try. socket id is ' + socket.id);
+
+  });
+  socket.on('disconnect', (reason) => {
+    if (logedIn) {
+      notify_user("You've been disconnected because " + reason + " !");
+      logedIn = false
+      document.querySelector('#status').classList.replace('text-success', 'text-warning');
+
+    }
+    console.log('you have been disconnected because ' + reason);
+  });
+
+
+
+
+
+
+});
